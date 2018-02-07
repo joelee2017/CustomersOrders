2c81f86e3154c95633a20cf8877cd25a334f9571
joelee2017 <as955133@gmail.com>
2018-02-07 12:04:06 +0800

    部分檢視(Partial View)與網頁區域更新 認識部份檢視(Partial View) 加入CustomersController控制器 編輯CustomersController使用的Index.cshtml

---
+++ CustomersOrders/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using CustomersOrders.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomersOrders.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            ViewBag.Customers = new SelectList(dc.Customers, "CustomerID", "CompanyName");
+            return View();
+        }
+    }
+}
