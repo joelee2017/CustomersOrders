df050a710bfafba646250543d236d01853d42136
joelee2017 <as955133@gmail.com>
2018-02-12 15:29:52 +0800

    將網頁使用的JavaScript製作成獨立的*.js檔案 如果JavaScript程式碼寫在獨立的檔案
    網站發佈功能可能應win更新版本造成失敗

---
+++ CustomersOrders/Scripts/site.js
@@ -0,0 +1,14 @@
+﻿$("#Customers").bind("change", function () {
+    UpdateOrder();
+});
+
+function UpdateOrder() {
+    $.ajax({
+        type: "GET",
+        url: webroot+"/"+ Customers.value,
+    }).done(function (result) {
+        $("#Orders").html(result);
+    }).fail(function (err) {
+        alert(err.responseText);
+    });
+}
